6372777f5214bb78aa1aa05d9d634cd632f261f3
Alexander Mertens <alexander.mertens@me.com>
2013-09-05 15:00:03 +0200

    Hello World Programm mit _ms_delay ausprogrammiert um zu überprüfen, ob F_CPU richtig gesetzt wird.

--- src/main.c
+++ src/main.c
@@ -6,13 +6,20 @@
  */
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
 	DDRC = 0xFF;
 
-	while(1) {
-		PORTC = 0b01010101;
+	uint8_t led = 1;
+
+	for(;;) {
+
+		PORTC = led;
+
+		led++;
+		_delay_ms(1000);
 	}
 }
