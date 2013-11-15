b16aaefe084b8339d4020060d34e94786f09b343
Alexander Mertens <alexander.mertens@me.com>
2013-11-15 12:55:30 +0100

    Funktionsprototypen angelegt.
    Alle wichtigen Funktionen sind benannt, allerdings sind die Parameter und Returns nicht nicht definiert.

--- src/md25.h
+++ src/md25.h
@@ -24,6 +24,31 @@
 /* external objects            */
 
 /* external functions          */
+extern void md25_set_speed(uint8, uint8); /*uint8 motor1, uint8 motor2 */
+extern void md25_set_motor1_speed();
+extern void md25_set_motor2_speed();
+
+extern void md25_get_motor2_speed();
+extern void md25_get_motor2_speed();
+
+extern void md25_set_acceleration_rate();
+extern void md25_get_acceleration_rate();
+
+extern void md25_set_mode();
+extern void md25_get_mode();
+
+extern void md25_get_motor_encoder_1();
+extern void md25_get_motor_encoder_2();
+
+extern void md25_get_battery_volts();
+extern void md25_get_motor1_current();
+extern void md25_get_motor2_current();
+extern void md25_get_software_revision();
+
+extern void md25_cmd_reset_encoders();
+extern void md25_cmd_enable_speed_regulation(bool);
+extern void md25_cmd_enable_i2c_timout(bool);
+//TODO: Implement functions for changing the i2c bus address
 
 #endif /* MD25_H_ */
 
