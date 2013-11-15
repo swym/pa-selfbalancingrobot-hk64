56b0cee929e6ba0ac094753f4a04559945911029
Alexander Mertens <alexander.mertens@me.com>
2013-11-15 12:23:10 +0100

    Low-Level Treiber Modul für den MD25 Motortreiber angelegt.

---
+++ src/md25.c
@@ -0,0 +1,18 @@
+/*
+ * md25.c
+ *
+ *  Created on: Nov 15, 2013
+ *      Author: alexandermertens
+ */
+
+
+/* *** INCLUDES ************************************************************** */
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* local type and constants     */
+
+/* local function declarations  */
+
+
+/* *** FUNCTION DEFINITIONS ************************************************** */
---
+++ src/md25.h
@@ -0,0 +1,31 @@
+/*
+ * md25.h
+ *
+ *  Created on: Nov 15, 2013
+ *      Author: alexandermertens
+ */
+
+
+#ifndef MD25_H_
+#define MD25_H_
+
+/* *** INCLUDES ************************************************************** */
+
+/* system headers              */ 
+
+/* local headers               */
+
+
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* external type and constants */
+
+/* external objects            */
+
+/* external functions          */
+
+#endif /* MD25_H_ */
+
+
+
