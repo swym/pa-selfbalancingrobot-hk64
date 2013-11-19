477909feeb427e43144c00058213a005ed8e75cd
Alexander Mertens <alexander.mertens@me.com>
2013-11-19 11:43:02 +0100

    Test-Modul für die Motorsteuerung angelegt.

---
+++ src/test/test_motor_control.c
@@ -0,0 +1,18 @@
+/*
+ * test_motor_control.c
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: alexandermertens
+ */
+
+
+/* *** INCLUDES ************************************************************** */
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* local type and constants     */
+
+/* local function declarations  */
+
+
+/* *** FUNCTION DEFINITIONS ************************************************** */
---
+++ src/test/test_motor_control.h
@@ -0,0 +1,31 @@
+/*
+ * test_motor_control.h
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: alexandermertens
+ */
+
+
+#ifndef TEST_MOTOR_CONTROL_H_
+#define TEST_MOTOR_CONTROL_H_
+
+/* *** INCLUDES ************************************************************** */
+
+/* system headers              */ 
+
+/* local headers               */
+
+
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* external type and constants */
+
+/* external objects            */
+
+/* external functions          */
+
+#endif /* TEST_MOTOR_CONTROL_H_ */
+
+
+
