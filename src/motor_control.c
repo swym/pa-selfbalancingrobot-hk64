ee5afdca93b868b3e3d09baa2d1910f51166f82d
Alexander Mertens <alexander.mertens@me.com>
2013-11-19 13:04:12 +0100

    Modul “motor_control.c” kommentiert

--- src/motor_control.c
+++ src/motor_control.c
@@ -21,12 +21,22 @@ static motor_contol_speed_t new_speed;
 
 
 /* *** FUNCTION DEFINITIONS ************************************************** */
+
+/**
+ * Saves the new calculated motor_speeds. To set the new speeds a call of
+ * motor_control_set_new_speed() must be performed.
+ * @param s
+ */
 void motor_control_prepare_new_speed(motor_contol_speed_t *s)
 {
 	new_speed.motor_1 = s->motor_1;
 	new_speed.motor_2 = s->motor_2;
 }
 
+/**
+ * Sets the new Motorspeed. Calls the correspondending driver-function of the
+ * md25. Also sets the struct current_speed = new_speed;
+ */
 void motor_control_set_new_speed()
 {
 	md25_set_speed(new_speed.motor_1, new_speed.motor_2);
@@ -35,12 +45,20 @@ void motor_control_set_new_speed()
 	current_speed.motor_2 = new_speed.motor_2;
 }
 
+/**
+ *Returns the current_speed struct
+ * @param s
+ */
 void motor_control_get_current_speed(motor_contol_speed_t *s)
 {
 	s->motor_1 = current_speed.motor_1;
 	s->motor_2 = current_speed.motor_2;
 }
 
+/**
+ * Returns the new_speed sturct
+ * @param s
+ */
 void motor_control_get_new_speed(motor_contol_speed_t *s)
 {
 	s->motor_1 = new_speed.motor_1;
