f2a8d9dbe3532bcd4fff9e82309c14b1624aa766
Alexander Mertens <alexander.mertens@me.com>
2013-11-19 18:00:00 +0100

    Init-Methode hinzugefügt
    Standard-Werte für new_speed und current_speed = 0
    MD25-init wird gerufen.

--- src/motor_control.c
+++ src/motor_control.c
@@ -21,6 +21,17 @@ static motor_contol_speed_t new_speed;
 
 
 /* *** FUNCTION DEFINITIONS ************************************************** */
+void motor_control_init()
+{
+	current_speed.motor_1 = 0;
+	current_speed.motor_2 = 0;
+
+	new_speed.motor_1 = 0;
+	new_speed.motor_2 = 0;
+
+	md25_init();
+}
+
 
 /**
  * Saves the new calculated motor_speeds. To set the new speeds a call of
