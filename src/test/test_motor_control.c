54a6137418d96953a52a5a7a774ae7e8a1024f5d
Alexander Mertens <alexander.mertens@me.com>
2013-11-19 17:45:57 +0100

    Test nochmal verbessert, indem nun auch motor_control_get_current_speed() verwendet wird.

--- src/test/test_motor_control.c
+++ src/test/test_motor_control.c
@@ -25,6 +25,7 @@
 void test_motor_control_set_different_speeds(void)
 {
 	motor_contol_speed_t speed;
+	motor_contol_speed_t printf_speed;
 
 	//init
 	md25_set_mode(MD25_MODE_SIGNED_SPEED);
@@ -32,15 +33,20 @@ void test_motor_control_set_different_speeds(void)
 	printf("test_motor_control_set_different_speeds(void)");
 	printf("init\n");
 
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
+
 	//1:neue Geschwindigkeit einstellen
 	//2:delay
 	//goto 1
-	printf("M1 = 0 , M2 = 0\n");
 	speed.motor_1 = 0;
 	speed.motor_2 = 0;
 
 	motor_control_prepare_new_speed(&speed);
 	motor_control_set_new_speed();
+
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
 
 
 	_delay_ms(2000.0);
@@ -49,13 +55,14 @@ void test_motor_control_set_different_speeds(void)
 	// SPEED 1
 	speed.motor_1 = 100;
 	speed.motor_2 = 100;
-	printf("M1 = %d , M2 = %d\n", speed.motor_1, speed.motor_2);
 
 	motor_control_prepare_new_speed(&speed);
 
 	_delay_ms(2000.0);
 
 	motor_control_set_new_speed();
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
 
 	_delay_ms(2000.0);
 
@@ -63,13 +70,14 @@ void test_motor_control_set_different_speeds(void)
 	// SPEED 1
 	speed.motor_1 = 0;
 	speed.motor_2 = 0;
-	printf("M1 = %d , M2 = %d\n", speed.motor_1, speed.motor_2);
 
 	motor_control_prepare_new_speed(&speed);
 
 	_delay_ms(1000.0);
 
 	motor_control_set_new_speed();
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
 
 	_delay_ms(2000.0);
 
@@ -77,52 +85,56 @@ void test_motor_control_set_different_speeds(void)
 	// SPEED 1
 	speed.motor_1 = 0;
 	speed.motor_2 = -100;
-	printf("M1 = %d , M2 = %d\n", speed.motor_1, speed.motor_2);
 
 	motor_control_prepare_new_speed(&speed);
 
 	_delay_ms(1000.0);
 
 	motor_control_set_new_speed();
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
 
 	_delay_ms(2000.0);
 
 	// SPEED 1
 	speed.motor_1 = -100;
 	speed.motor_2 = 0;
-	printf("M1 = %d , M2 = %d\n", speed.motor_1, speed.motor_2);
 
 	motor_control_prepare_new_speed(&speed);
 
 	_delay_ms(1000.0);
 
 	motor_control_set_new_speed();
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
 
 	_delay_ms(2000.0);
 
 	// SPEED 1
 	speed.motor_1 = -100;
 	speed.motor_2 = 100;
-	printf("M1 = %d , M2 = %d\n", speed.motor_1, speed.motor_2);
 
 	motor_control_prepare_new_speed(&speed);
 
 	_delay_ms(1000.0);
 
 	motor_control_set_new_speed();
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
 
 	_delay_ms(2000.0);
 
 	// SPEED 1
 	speed.motor_1 = 0;
 	speed.motor_2 = 0;
-	printf("M1 = %d , M2 = %d\n", speed.motor_1, speed.motor_2);
 
 	motor_control_prepare_new_speed(&speed);
 
 	_delay_ms(1000.0);
 
 	motor_control_set_new_speed();
+	motor_control_get_current_speed(&printf_speed);
+	printf("current: M1 = %d , M2 = %d\n",printf_speed.motor_1, printf_speed.motor_2);
 
 	_delay_ms(2000.0);
 
