1c97d215c525164eae642b237a601a7116933491
Alexander Mertens <alexander.mertens@me.com>
2013-11-20 11:41:43 +0100

    Modul “controller.c” angelegt. Dies soll übergangsweise die timergesteuerte Motorsteuerung implementieren

---
+++ src/controller.c
@@ -0,0 +1,33 @@
+/*
+ * controller.c
+ *
+ *  Created on: Nov 20, 2013
+ *      Author: alexandermertens
+ */
+
+
+/* *** INCLUDES ************************************************************** */
+#include "controller.h"
+
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* local type and constants     */
+
+/* local function declarations  */
+
+
+/* *** FUNCTION DEFINITIONS ************************************************** */
+void controller_init(void)
+{
+
+}
+
+void controller_run(void)
+{
+	for(;;) {
+
+	}
+}
+
+
