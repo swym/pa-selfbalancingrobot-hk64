c4f6cdf276e4b511ceb40edaa08633822949e9bd
Alexander Mertens <alexander.mertens@me.com>
2013-11-22 17:31:18 +0100

    4ms Slots

--- src/controller.c
+++ src/controller.c
@@ -15,8 +15,19 @@
 /* *** DECLARATIONS ********************************************************** */
 
 /* local type and constants     */
+#define SLOT_COUNTER_MAX 4 // (IntervalllŠnge = 20 ms / IRQ-Zeit = 4 ms)-1 -> 5-1
+
+#define LED1 1
+#define LED2 2
+#define LED3 4
+
 volatile bool timer0_compare_reached = false;
+volatile uint8_t slot_cnt = 0;
+
 /* local function declarations  */
+
+
+/* *** FUNCTION DEFINITIONS ************************************************** */
 
 /*
 				Timer0 wird auf eine PeriodenlŠnge von T = 4 ms einstellt
@@ -33,8 +44,6 @@ volatile bool timer0_compare_reached = false;
 	Vgl.:	Atmel-2490-8-bit-AVR-Microcontroller-ATmega64-L_datasheet.pdf
 			Seite 125
 */
-
-/* *** FUNCTION DEFINITIONS ************************************************** */
 void controller_init(void)
 {
 	/* Timer0 wird auf eine PeriodenlŠnge von T = 4 ms einstellt */
@@ -50,20 +59,56 @@ void controller_init(void)
 										//Output Compare Match Interrupt Enable
 
 	//Timer0 OCR-Register vorladen
-	OCR0 = 124;						//OCR0: Output Compare Register
+	OCR0 = 249;						//OCR0: Output Compare Register
 }
 
 void controller_run(void)
 {
 	uint8_t led_value = 0;
 
+
 	for(;;) {
+
+
+
+
+		/* Bei Interrupt Slot-Counter herhšhen */
+//		if(timer0_compare_reached) {
+//
+//			timer0_compare_reached = false;
+//		}
 
 		if(timer0_compare_reached) {
 
-			led_value++;
-
 			timer0_compare_reached = false;
+			//led_value ^= LED3;
+
+			/* Erster Zeitslot am Anfang des Intervalls; t = 0 ms */
+			if(slot_cnt == 0) {
+				/*
+				 * Beschleunigungswerte lesen
+				 * Beschleunigungswerte in Position umrechnen
+				 * als IST-Wert in den PID-Regler geben
+				 * Stellgrš§e an Motorsteuerung weitergeben, ABER noch nicht setzen
+				 */
+				led_value ^= LED1;
+			}
+
+			/* Erster Zeitslot am Anfang des Intervalls; t = 12 ms */
+			if(slot_cnt == 2) {
+
+				/*
+				 * Neue Stellgrš§e des Motors setzen
+				 */
+				led_value ^= (LED1 | LED2);
+
+			}
+
+			if(slot_cnt >= SLOT_COUNTER_MAX) {
+				led_value &= ~(LED1 | LED2);
+
+
+			}
 		}
 		PORTC = led_value;
 	}
@@ -71,5 +116,11 @@ void controller_run(void)
 
 ISR(TIMER0_COMP_vect)
 {
+	if(slot_cnt >= SLOT_COUNTER_MAX) {
+		slot_cnt = 0;
+		PORTC ^= LED3;
+	} else {
+		slot_cnt++;
+	}
 	timer0_compare_reached = true;
 }
