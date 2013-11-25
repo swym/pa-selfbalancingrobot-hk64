9bac71a4f8c3b6d3aad3c02c498e3ddc12dc00d9
Alexander Mertens @ Dorobook <alexander.mertens@me.com>
2013-11-25 17:29:59 +0100

    Modul für den Hardware-Timer angelegt

---
+++ src/timer.c
@@ -0,0 +1,15 @@
+/*
+ * timer.c
+ *
+ *  Created on: 25.11.2013
+ *      Author: alexandermertens
+ */
+
+
+#include "timer.h"
+
+
+void timer_init()
+{
+
+}
---
+++ src/timer.h
@@ -0,0 +1,19 @@
+/*
+ * timer.h
+ *
+ *  Created on: 25.11.2013
+ *      Author: alexandermertens
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+
+
+#endif /* TIMER_H_ */
+
+
+//External Data
+
+//External Functions
+extern void timer_init(void);
