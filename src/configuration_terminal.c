267915be52e6e0d52fb72fd2a512e0224bb7762e
Alexander Mertens @ Dorobook <alexander.mertens@me.com>
2013-11-28 14:34:53 +0100

    Den einfachen Zustandsautomaten um eine Hilfsfunktion zur identifitierung der Eingabe erweitert (configuration_terminal_get_choice())

--- src/configuration_terminal.c
+++ src/configuration_terminal.c
@@ -7,11 +7,18 @@
 
 /* *** INCLUDES ************************************************************** */
 #include "configuration_terminal.h"
+#include "lib/uart.h"
 
 #include <util/delay.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 /* *** DECLARATIONS ********************************************************** */
+
+//-------------------Defines--------------------//
+
+#define INPUT_BUFFER_SIZE 8
 
 /* local type and constants     */
 typedef enum {
@@ -22,9 +29,15 @@ typedef enum {
 
 static configuration_terminal_state_t current_state = STATE_MAIN_MENU;
 
-//-------------------Defines--------------------//
+static char input_buffer[INPUT_BUFFER_SIZE];
+
+
 
 /* local function declarations  */
+void configuration_terminal_clear(void);
+char configuration_terminal_get_choice(void);
+
+/* states */
 void configuration_terminal_state_main_menu(void);
 void configuration_terminal_state_setPID(void);
 void configuration_terminal_state_tata_acceleration(void);
@@ -59,21 +72,77 @@ void configuration_terminal_state_machine(void)
 
 void configuration_terminal_state_main_menu(void)
 {
-	printf("MAIN MENU\n");
 
+
+	//ENTRY
+	configuration_terminal_clear();
+
+	printf("MAIN MENU\n\n[H]odensack:\n[I]n kalte\n[K]acke");
+
+	//DO
+
+	for(;;)
+	{
+		while(!UART_char_received()) {
+			printf(".");
+			_delay_ms(1000.0);
+		}
+		UART_clr_rx_buf();
+
+		printf("\nLoop interrupted\nPlease type char:\n");
+
+		printf("input: %c\n", configuration_terminal_get_choice());
+
+	}
+	//EXIT
 	current_state = STATE_SETPID;
 }
 
 void configuration_terminal_state_setPID(void)
 {
-	printf("MAIN SET PID\n");
+	configuration_terminal_clear();
+
+	printf("MENU SET PID\n");
 
 	current_state = STATE_TARA_ACCEL;
 }
 
 void configuration_terminal_state_tata_acceleration(void)
 {
-	printf("MAIN TARA ACCELERATIONSENSOR\n");
+	configuration_terminal_clear();
+
+	printf("MENU TARA ACCELERATIONSENSOR\n");
 
 	current_state = STATE_MAIN_MENU;
+
 }
+
+void configuration_terminal_clear(void)
+{
+	/* http://www.avrfreaks.net/index.php?name=PNphpBB2&file=printview&t=80382&start=0 */
+	char cmd[] = {27, '[', '2', 'J', '\0'};
+
+	printf(cmd);
+}
+
+char configuration_terminal_get_choice(void)
+{
+	char choice = 0;
+
+	fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
+
+	choice = input_buffer[0];
+
+	if(!isalpha(choice)) {
+		choice = 0;
+	}
+
+	//Überlangen string lesen und verwerfen
+	while(strlen(input_buffer) >= INPUT_BUFFER_SIZE - 1) {
+		fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
+	}
+
+	return choice;
+}
+
+
