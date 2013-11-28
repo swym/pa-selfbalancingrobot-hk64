f940ae45919e279bca406b36f2dfdadb6e129c34
Alexander Mertens @ Dorobook <alexander.mertens@me.com>
2013-11-28 11:36:31 +0100

    Modul "configuration_terminal.c" angelegt.

---
+++ src/configuration_terminal.c
@@ -0,0 +1,9 @@
+/*
+ * configuration_terminal.c
+ *
+ *  Created on: 28.11.2013
+ *      Author: alexandermertens
+ */
+
+
+#include "configuration_terminal.h"
---
+++ src/configuration_terminal.h
@@ -0,0 +1,13 @@
+/*
+ * configuration_terminal.h
+ *
+ *  Created on: 28.11.2013
+ *      Author: alexandermertens
+ */
+
+#ifndef CONFIGURATION_TERMINAL_H_
+#define CONFIGURATION_TERMINAL_H_
+
+
+
+#endif /* CONFIGURATION_TERMINAL_H_ */
