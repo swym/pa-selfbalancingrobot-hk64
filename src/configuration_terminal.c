20829f6f14a48ef8b9724a6e0878a66247810cba
Alexander Mertens @ Dorobook <alexander.mertens@me.com>
2013-11-29 13:18:34 +0100

    Weitere Hilfsmethoden zur Steuerung des Terminals nach VT100 hinzugefügt
    void configuration_terminal_clear_screen(void);
    void configuration_terminal_set_cursor_top_left(void);
    char configuration_terminal_get_choice(void);
    void configuration_terminal_set_cursor_on_position(uint8_t, uint8_t); /* uint8_t x, uint8_t y */

--- src/configuration_terminal.c
+++ src/configuration_terminal.c
@@ -19,6 +19,7 @@
 //-------------------Defines--------------------//
 
 #define INPUT_BUFFER_SIZE 8
+#define ASCII_ESC		  (char)(27)
 
 /* local type and constants     */
 typedef enum {
@@ -34,8 +35,12 @@ static char input_buffer[INPUT_BUFFER_SIZE];
 
 
 /* local function declarations  */
-void configuration_terminal_clear(void);
+void configuration_terminal_clear_screen(void);
+void configuration_terminal_set_cursor_top_left(void);
 char configuration_terminal_get_choice(void);
+void configuration_terminal_set_cursor_on_position(uint8_t, uint8_t); /* uint8_t x, uint8_t y */
+
+void configuration_terminal_clear_input_buffer(void);
 
 /* states */
 void configuration_terminal_state_main_menu(void);
@@ -48,7 +53,9 @@ void configuration_terminal_state_tata_acceleration(void);
 void configuration_terminal_state_machine(void)
 {
 
-	for(;;) {
+	configuration_terminal_clear_screen();
+
+ 	for(;;) {
 
 		switch(current_state) {
 
@@ -75,32 +82,33 @@ void configuration_terminal_state_main_menu(void)
 
 
 	//ENTRY
-	configuration_terminal_clear();
+	configuration_terminal_clear_screen();
+	configuration_terminal_set_cursor_top_left();
+	configuration_terminal_clear_input_buffer();
 
 	printf("MAIN MENU\n\n[H]odensack:\n[I]n kalte\n[K]acke");
 
 	//DO
 
-	for(;;)
-	{
-		while(!UART_char_received()) {
-			printf(".");
-			_delay_ms(1000.0);
-		}
-		UART_clr_rx_buf();
 
-		printf("\nLoop interrupted\nPlease type char:\n");
-
-		printf("input: %c\n", configuration_terminal_get_choice());
-
+	while(!UART_char_received()) {
+		printf(".");
+		_delay_ms(1000.0);
 	}
+	UART_clr_rx_buf();
+
+	printf("\nLoop interrupted\nPlease type char:\n");
+
+	printf("input: %c\n", configuration_terminal_get_choice());
+
+
 	//EXIT
 	current_state = STATE_SETPID;
 }
 
 void configuration_terminal_state_setPID(void)
 {
-	configuration_terminal_clear();
+	configuration_terminal_clear_screen();
 
 	printf("MENU SET PID\n");
 
@@ -109,40 +117,85 @@ void configuration_terminal_state_setPID(void)
 
 void configuration_terminal_state_tata_acceleration(void)
 {
-	configuration_terminal_clear();
+	configuration_terminal_clear_screen();
+	configuration_terminal_set_cursor_on_position(10, 5);
 
 	printf("MENU TARA ACCELERATIONSENSOR\n");
 
+	_delay_ms(3000.0);
+
 	current_state = STATE_MAIN_MENU;
 
-}
-
-void configuration_terminal_clear(void)
-{
-	/* http://www.avrfreaks.net/index.php?name=PNphpBB2&file=printview&t=80382&start=0 */
-	char cmd[] = {27, '[', '2', 'J', '\0'};
-
-	printf(cmd);
 }
 
 char configuration_terminal_get_choice(void)
 {
 	char choice = 0;
 
+	//Eingabe einlesen
 	fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
 
+	//Nur den ersten Char verarbeiten
 	choice = input_buffer[0];
 
-	if(!isalpha(choice)) {
+	//Wertebereichprüfung und convertierung zu Großbuchstaben
+	if(isalpha(choice)) {
+		choice = toupper(choice);
+	} else {
 		choice = 0;
 	}
 
-	//Überlangen string lesen und verwerfen
-	while(strlen(input_buffer) >= INPUT_BUFFER_SIZE - 1) {
-		fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
-	}
+	//Buffer leeren
+	configuration_terminal_clear_input_buffer();
 
 	return choice;
 }
 
+void configuration_terminal_clear_input_buffer(void)
+{
+	//den eventuell noch gefüllten Sendebuffer des PC lesen und verwerfen
+	while(strlen(input_buffer) >= INPUT_BUFFER_SIZE - 1) {
+		fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
+	}
+
+	//Buffer als "leer" markieren
+	input_buffer[0] = '\0';
+}
+
+
+void configuration_terminal_clear_screen(void)
+{
+	/* http://www.avrfreaks.net/index.php?name=PNphpBB2&file=printview&t=80382&start=0 */
+	char cmd[] = {ASCII_ESC, '[', '2', 'J', '\0'};
+
+	printf(cmd);
+}
+
+void configuration_terminal_set_cursor_top_left(void)
+{
+	/* http://www-user.tu-chemnitz.de/~heha/hs/terminal/terminal.htm */
+	/* http://ascii-table.com/ansi-escape-sequences-vt-100.php */
+	char cmd[] = {ASCII_ESC, '[', 'H', '\0'};
+
+	printf(cmd);
+}
+
+void configuration_terminal_set_cursor_on_position(uint8_t line, uint8_t column)
+{
+	char cmd[50];
+
+	//check range of line; set to 1 if invalid
+	if(line < 1) {
+		line = 1;
+	}
+
+
+	//check range of line; set to 1 if invalid
+	if(column < 1) {
+		column = 1;
+	}
+
+	sprintf(cmd, "%c[%d;%dH", ASCII_ESC, line, column);
+	printf(cmd);
+}
 
