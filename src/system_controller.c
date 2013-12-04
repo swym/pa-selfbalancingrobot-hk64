9bb9fd570f7b30a9089cf8b18123dc916775f0d7
Alexander Mertens <alexander.mertens@me.com>
2013-12-04 15:29:19 +0100

    Das Modul “system-controller” angelegt. Es soll alle einzelnen Systemomponenten zusammenführen und mit Hilfe eine Zustandaustoatens steuern.

---
+++ src/system_controller.c
@@ -0,0 +1,19 @@
+/*
+ * system_controller.c
+ *
+ *  Created on: Dec 4, 2013
+ *      Author: alexandermertens
+ */
+
+
+/* *** INCLUDES ************************************************************** */
+#include "system_controller.h"
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* local type and constants     */
+
+/* local function declarations  */
+
+
+/* *** FUNCTION DEFINITIONS ************************************************** */
---
+++ src/system_controller.h
@@ -0,0 +1,31 @@
+/*
+ * system_controller.h
+ *
+ *  Created on: Dec 4, 2013
+ *      Author: alexandermertens
+ */
+
+
+#ifndef SYSTEM_CONTROLLER_H_
+#define SYSTEM_CONTROLLER_H_
+
+/* *** INCLUDES ************************************************************** */
+
+/* system headers              */ 
+
+/* local headers               */
+
+
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* external type and constants */
+
+/* external objects            */
+
+/* external functions          */
+
+#endif /* SYSTEM_CONTROLLER_H_ */
+
+
+
