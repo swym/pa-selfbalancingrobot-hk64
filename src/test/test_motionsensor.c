f6a45bd7824d3e5a4e341a6edac62cba5dd460cd
Alexander Mertens <alexander.mertens@me.com>
2014-06-29 12:13:30 +0200

    Test Modul für den Motionsensor angesst... *blah*

--- src/test/test_motionsensor.c
+++ src/test/test_motionsensor.c
@@ -49,34 +49,37 @@ void test_motionsensor_init()
 	sei();
 
 	motionsensor_init();
+	motionsensor_set_zero_point();
 }
 
 void test_motionsensor_run()
 {
 	test_motionsensor_init();
 
+
 	printf("test_motionsensor_run\n");
+
+
+	rotation_t rot_vector;
+	acceleration_t accel_vector;
 
 	while(true) {
 
-		rotation_t rot_vector;
-		acceleration_t accel_vector;
+		PORTC ^= _BV(PC7);
 
-		while(true) {
+		motionsensor_get_current_acceleration(&accel_vector);
+		motionsensor_get_current_rotation(&rot_vector);
 
-			PORTC ^= _BV(PC7);
+		printf("%d:%d:%d:%d:%d:%d:",
+				accel_vector.x, accel_vector.y, accel_vector.z,
+				rot_vector.x,rot_vector.y, rot_vector.z);
 
-			motionsensor_get_current_acceleration(&accel_vector);
-			motionsensor_get_current_rotation(&rot_vector);
+		printf("%f\n",motionsensor_get_position());
 
-			printf("%d:%d:%d:%d:%d:%d:\n",
-					accel_vector.x, accel_vector.y, accel_vector.z,
-					rot_vector.x,rot_vector.y, rot_vector.z);
 
-			//printf("pos: %f\n", motionsensor_get_position());
+		//printf("pos: %f\n", motionsensor_get_position());
 
-			_delay_ms(5);
-		}
+//		_delay_ms(1);
 	}
 }
 
