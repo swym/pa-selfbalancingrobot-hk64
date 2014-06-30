cdd18bca27ce35900eeca723fc4c9a061ec813b1
Alexander Mertens <alexander.mertens@me.com>
2014-06-30 11:34:45 +0200

    Test Modul zum Laden und Speichern angelegt

---
+++ src/test/test_configuration_storage.h
@@ -0,0 +1,27 @@
+/*
+ * test_configuration_storage.h
+ *
+ *  Created on: Jun 29, 2014
+ *      Author: alexandermertens
+ */
+
+
+#ifndef TEST_CONFIGURATION_STORAGE_H_
+#define TEST_CONFIGURATION_STORAGE_H_
+
+/* *** INCLUDES ************************************************************** */
+
+/* * system headers              * */
+
+/* * local headers               * */
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* * external type and constants * */
+
+/* * external objects            * */
+
+/* * external functions          * */
+extern void test_configuration_storage_run(void);
+
+#endif /* TEST_CONFIGURATION_STORAGE_H_ */
