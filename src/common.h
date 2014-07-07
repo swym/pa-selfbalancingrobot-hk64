7b428dbc8219aa658a0331b82eb5546c6f06c0c6
Alexander Mertens <alexander.mertens@me.com>
2014-07-07 15:22:38 +0200

    TWI Bug: Manchmal verbleibt die Funktion twi_send_data() in der while(!ready) Schleife. bricht auch ab, wenn einer Timer abläuft. Jedoch ist seit dem das Problem nicht mehr aufgetreten.

---
+++ src/common.h
@@ -0,0 +1,31 @@
+/*
+ * common.h
+ *
+ *  Created on: Jul 7, 2014
+ *      Author: alexandermertens
+ */
+
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+/* *** INCLUDES ************************************************************** */
+
+/* * system headers              * */
+
+/* * local headers               * */
+
+/* *** DECLARATIONS ********************************************************** */
+
+/* * external type and constants * */
+#define PORT_LED	PORTF
+#define DDR_LED		DDRF
+
+#define PORT_SCOPE	PORTC
+#define DDR_SCOPE	DDRC
+
+/* * external objects            * */
+
+/* * external functions          * */
+
+#endif /* COMMON_H_ */
