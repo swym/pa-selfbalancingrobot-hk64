8f1832e74d01842e4e14e3ed536f917d907ce42a
Alexander Mertens <alexander.mertens@me.com>
2014-07-09 11:26:21 +0200

    rotation_t heißt jetzt korrekterweise angularvelocity_t

--- src/motionsensor.c
+++ src/motionsensor.c
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <stdio.h>
+
 /* * local headers               * */
 //#include "bma020.h"
 #include "mpu9150.h"
@@ -21,7 +23,10 @@
 
 
 /* *** DEFINES ************************************************************** */
+#define RAD2DEG 5729.57
 
+#define COMPFILTER_GRYO_FACTOR  0.95
+#define COMPFILTER_ACCEL_FACTOR 0.05
 
 /* *** DECLARATIONS ********************************************************* */
 
@@ -30,7 +35,7 @@
 /* * local objects               * */
 
 static acceleration_t acceleration_offset;
-static rotation_t rotation_offset;
+static angularvelocity_t angularvelocity_offset;
 
 static double position_multiplier;
 
@@ -43,7 +48,7 @@ static moving_average_t average_acceleration_y;
 static moving_average_t average_acceleration_z;
 
 static int16_t gyro_angle_y;
-static int16_t accel_angle_y;
+
 
 /* * local function declarations * */
 //static void motionsensor_get_current_rotation(rotation_t *rotation);
@@ -79,48 +84,52 @@ static int16_t accel_angle_y;
 
 int16_t motionsensor_get_position()
 {
+	int16_t angularvelocity_y;  //neue winkelgeschwindigkeit
+	int16_t accel_angle_y;
 	int16_t angle_y;
-
-	//update sensorwerte
-	rotation_t cur_rotation;
 	acceleration_t cur_acceleration;
 
-	motionsensor_get_current_rotation(&cur_rotation);
+	//motionsensor_get_current_rotation(&cur_rotation);
+	angularvelocity_y = mpu9150_read_angularvelocity_y() + 350;
 	motionsensor_get_current_acceleration(&cur_acceleration);
 
-	//integrate angular velocity to angle over time (dt = 20 ms)
+	//integrate angular velocity to angle over time (dt = 4 ms)
+	gyro_angle_y += (angularvelocity_y * 4/1000);
 
-	gyro_angle_y += (cur_rotation.y * 20/1000);
-	accel_angle_y = (int16_t)(1000*(atan2(cur_acceleration.x, cur_acceleration.z)));
+	//determine angle using acceleration vectors and atan
+	accel_angle_y = (int16_t)((atan2(cur_acceleration.x, cur_acceleration.z) * RAD2DEG));
+
+	//sensordata fusion with a complementary filter
+	//angle_y = (0.3 * gyro_angle_y) + (0.7 * accel_angle_y); //gut, aber sehr verrauscht
+	angle_y = (COMPFILTER_GRYO_FACTOR * gyro_angle_y) + (COMPFILTER_ACCEL_FACTOR * accel_angle_y);
 
 
 
-	//Complementary Filter
-	angle_y = (0.9 * gyro_angle_y) + (0.1 * accel_angle_y);
+//	printf("% 5d% 5d% 5d\n",gyro_angle_y, accel_angle_y, angle_y);
 
 	return angle_y;
 
 }
 
-void motionsensor_get_current_rotation(rotation_t *rotation)
+void motionsensor_get_current_rotation(angularvelocity_t *angularvelocity)
 {
-	rotation_t new_rotation;
+	angularvelocity_t new_angularvelocity;
 
-	mpu9150_read_rotation(&new_rotation);
+	mpu9150_read_angularvelocity(&new_angularvelocity);
 
 	//do offset calculation?
-	new_rotation.x += 0;
-	new_rotation.y += 350;
-	new_rotation.z += 0;
+	new_angularvelocity.x += 0;
+	new_angularvelocity.y += 350;
+	new_angularvelocity.z += 0;
 
-	moving_average_simple_put_element(&average_rotation_x, new_rotation.x);
-	moving_average_simple_put_element(&average_rotation_y, new_rotation.y);
-	moving_average_simple_put_element(&average_rotation_z, new_rotation.z);
+	moving_average_simple_put_element(&average_rotation_x, new_angularvelocity.x);
+	moving_average_simple_put_element(&average_rotation_y, new_angularvelocity.y);
+	moving_average_simple_put_element(&average_rotation_z, new_angularvelocity.z);
 
 	//prepare acceleration struct
-	rotation->x = average_rotation_x.mean;
-	rotation->y = average_rotation_y.mean;
-	rotation->z = average_rotation_z.mean;
+	angularvelocity->x = average_rotation_x.mean;
+	angularvelocity->y = average_rotation_y.mean;
+	angularvelocity->z = average_rotation_z.mean;
 }
 void motionsensor_get_current_acceleration(acceleration_t *acceleration)
 {
@@ -207,6 +216,10 @@ void motionsensor_set_zero_point(void)
 	acceleration_offset.x = -tmp_acceleration.x;
 	acceleration_offset.y = -tmp_acceleration.y;
 	acceleration_offset.z = (INT16_MAX / 2) -tmp_acceleration.z;
+
+
+	angularvelocity_offset.x = -
+
 }
 
 void motionsensor_init()
