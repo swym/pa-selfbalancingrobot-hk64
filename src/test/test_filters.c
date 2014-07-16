4ab10e91e8a4869b9436f9bce8722260ac6787bc
Alexander Mertens <alexander.mertens@me.com>
2014-07-16 18:37:25 +0200

    Test Modul für Filters testet beide Mittelwerte

--- src/test/test_filters.c
+++ src/test/test_filters.c
@@ -30,7 +30,8 @@
 /* * local type and constants    * */
 
 /* * local objects               * */
-weighted_average_t avg;
+weighted_average_t wavg;
+moving_average_t   mavg;
 
 /* * local function declarations * */
 
@@ -42,19 +43,45 @@ void test_filters_init()
 	DDR_SCOPE = 0xFF;
 
 	UART_init(56700);	/* Init UART mit 38400 baud */
+
+	uint8_t i;
+
+	for(i = 0;i < WEIGHTED_AVERAGE_ELEMENT_COUNT * 10;i++) {
+		filters_weighted_average_put_element(&wavg, 0);
+		filters_moving_average_put_element(&mavg, 0);
+	}
+
 }
 
 void test_filters_run()
 {
 	test_filters_init();
 
-	printf("hello filters\n");
+	printf("test filters\n");
 
-	filters_weighted_average_put_element(&avg, 1);
-	filters_weighted_average_put_element(&avg, 1);
-	filters_weighted_average_put_element(&avg, 1);
-	filters_weighted_average_put_element(&avg, 1);
-	filters_weighted_average_put_element(&avg, 1);
 
-	printf("wavg: %d\n", avg.mean);
+	PORT_SCOPE = 0x01;
+	filters_weighted_average_put_element(&wavg, 16660);
+	PORT_SCOPE = 0x02;
+	filters_moving_average_put_element(&mavg, 16660);
+	PORT_SCOPE = 0x00;
+	printf("\n");
+	filters_weighted_average_put_element(&wavg, 17000);
+	filters_moving_average_put_element(&mavg, 17000);
+	printf("\n");
+	filters_weighted_average_put_element(&wavg, 10000);
+	filters_moving_average_put_element(&mavg, 10000);
+	printf("\n");
+	filters_weighted_average_put_element(&wavg, 13344);
+	filters_moving_average_put_element(&mavg, 13344);
+	printf("\n");
+	PORT_SCOPE = 0x01;
+	filters_weighted_average_put_element(&wavg, 16660);
+	PORT_SCOPE = 0x02;
+	filters_moving_average_put_element(&mavg, 16660);
+	PORT_SCOPE = 0x00;
+	printf("\n");
+
+	printf("wavg: %d\n", wavg.mean);
+	printf("mavg: %d\n", mavg.mean);
 }
