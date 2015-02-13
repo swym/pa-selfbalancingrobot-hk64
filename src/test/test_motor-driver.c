1683a0a5b03de92cd70121d02be49028e51e3cad
Alexander Mertens <alexander.mertens@me.com>
2015-02-13 14:20:29 +0100

    test angepasst

--- src/test/test_motor-driver.c
+++ src/test/test_motor-driver.c
@@ -9,10 +9,17 @@
 /* *** INCLUDES ************************************************************* */
 
 /* * system headers              * */
+#include <stdio.h>
+#include <stdbool.h>
+
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 /* * local headers               * */
 #include "../l6205pd.h"
+#include "../timer.h"
+#include "../common.h"
+#include "../lib/uart.h"
 
 
 /* *** DEFINES ************************************************************** */
@@ -23,6 +30,8 @@
 /* * local type and constants    * */
 
 /* * local objects               * */
+static uint16_t motor_speed_timer;
+static uint8_t led_value;
 
 /* * local function declarations * */
 void test_motor_driver_init(void);
@@ -35,31 +44,77 @@ void test_motor_driver(void)
 {
 	test_motor_driver_init();
 
-	uint8_t i;
+	bool set_speed = true;
+
+	for(;;) {
+
+		if(timer_slot_0) {
+
+			if(set_speed) {
+
+				motor_set_speed(MOTOR_1, 255);
+
+				set_speed = false;
+			}
+		}
+
+			/*
+
+			motor_speed_timer++;
+
+			if(motor_speed_timer == 1) {
 
 
-	for(i = 0;i < 255;i++) {
-		motor_set_speed(MOTOR_1, 128);
-		motor_set_speed(MOTOR_2, 128);
+				printf("step1\n");
+				PORTA = led_value++;
 
-		_delay_ms(2000);
+				motor_set_speed(MOTOR_1, 128);
+				motor_set_speed(MOTOR_2, 128);
+			}
 
-		motor_set_speed(MOTOR_1, 0);
-		motor_set_speed(MOTOR_2, 0);
+			if(motor_speed_timer == 20480) {
 
-		_delay_ms(2000);
+				printf("step2\n");
+				PORTA = led_value++;
 
-		motor_set_speed(MOTOR_1, 255);
-		motor_set_speed(MOTOR_2, 255);
+				motor_set_speed(MOTOR_1, 0);
+				motor_set_speed(MOTOR_2, 0);
+			}
 
-		_delay_ms(2000);
+			if(motor_speed_timer == 40960) {
 
+				printf("step3\n");
+				PORTA = led_value++;
+
+				motor_set_speed(MOTOR_1, 255);
+				motor_set_speed(MOTOR_2, 255);
+			}
+
+
+			if(motor_speed_timer > 40960) {
+
+
+				motor_speed_timer = 0;
+			}
+
+		}
+
+		*/
+
+		motor_update_pwm();
 	}
 }
 
 void test_motor_driver_init(void)
 {
+	timer_init();
 	init_motors();
 
+	UART_init(57600);
+
+	DDRA = 0xFF;
+
 	sei();
+
+	printf("inited...\n");
 }
