1cf5a187a0e70740d20988c4ac294cbebfa85f86
Alexander Mertens <alexander.mertens@me.com>
2015-02-13 10:59:07 +0100

    Testaufrufe für den Motortreiber hinzugefügt.

--- src/test/test_motor-driver.c
+++ src/test/test_motor-driver.c
@@ -9,8 +9,10 @@
 /* *** INCLUDES ************************************************************* */
 
 /* * system headers              * */
+#include <util/delay.h>
 
 /* * local headers               * */
+#include "../l6205pd.h"
 
 
 /* *** DEFINES ************************************************************** */
@@ -23,11 +25,41 @@
 /* * local objects               * */
 
 /* * local function declarations * */
+void test_motor_driver_init(void);
 
 
 /* *** FUNCTION DEFINITIONS ************************************************* */
 
+
 void test_motor_driver(void)
 {
-	//stub
+	test_motor_driver_init();
+
+	uint8_t i;
+
+
+	for(i = 0;i < 255;i++) {
+		motor_set_speed(MOTOR_1, 128);
+		motor_set_speed(MOTOR_2, 128);
+
+		_delay_ms(2000);
+
+		motor_set_speed(MOTOR_1, 0);
+		motor_set_speed(MOTOR_2, 0);
+
+		_delay_ms(2000);
+
+		motor_set_speed(MOTOR_1, 255);
+		motor_set_speed(MOTOR_2, 255);
+
+		_delay_ms(2000);
+
+	}
+}
+
+void test_motor_driver_init(void)
+{
+	init_motors();
+
+	sei();
 }
