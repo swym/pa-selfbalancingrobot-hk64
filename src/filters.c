157aef4d2b77c101f61ddbe424c426e6752702e5
Alexander Mertens <alexander.mertens@me.com>
2015-02-26 11:22:03 +0100

    Das Filter Modul aufgeräumt  - alte "Gewichtete Mittelwertsfunktion entfernt  - init und flush Methoden für den einfachen Mittelwert hinzugefügt.  - Debugausgaben entfernt.

--- src/filters.c
+++ src/filters.c
@@ -10,7 +10,6 @@
 #include "filters.h"
 
 /* * system headers              * */
-#include <stdio.h>
 
 /* * local headers               * */
 
@@ -23,18 +22,39 @@
 /* * local type and constants    * */
 
 /* * local objects               * */
-static uint8_t weighted_average_weights[] = {3, 2, 1, 0, 0};	//element << weights[i] -> element * (8, 4, 2, 1, 1)
-static uint8_t weighted_average_devisor = 4;				//sum of left shifted weights
-/* * local function declarations * */
 
+/* * local function declarations * */
 
 /* *** FUNCTION DEFINITIONS ************************************************* */
 
-//8, 2, 1, 0, 0, 0, 0, 0;
+
+void filters_moving_generic_average_put_element(
+		filters_moving_generic_average_t * average,
+		int16_t new_value)
+{
+	uint8_t i;
+	int32_t elements_sum = 0;
+
+	// iterate over the (weights - 1) to aging existing elements
+	for(i = average->weights_count - 1; i > 0; i--) {
+		// move elements to left (aging them; drop last element)
+		average->elements[i] = average->elements[i - 1];
+		// elements_sum them up with their weight
+		elements_sum += ((int32_t)average->elements[i]) * average->weights[i];
+	}
+	//add newest element, apply weight to element and add to sum
+	average->elements[0] = new_value;
+	elements_sum += ((int32_t)average->elements[0]) * average->weights[0];
+
+	//divide elements_sum with weights_sum and save calculated average in structure
+	average->avg = (int16_t)(elements_sum / average->weights_sum);
+}
+
+
 void filters_moving_generic_average_init(
 		filters_moving_generic_average_t * average,
 		uint8_t * weights,
-		uint16_t init_value)
+		int16_t init_value)
 {
 	uint8_t i;
 
@@ -56,7 +76,8 @@ void filters_moving_generic_average_init(
 	}
 }
 
-void filters_moving_generic_average_flush(filters_moving_generic_average_t * average)
+void filters_moving_generic_average_flush(
+		filters_moving_generic_average_t * average)
 {
 	uint8_t i;
 
@@ -65,74 +86,52 @@ void filters_moving_generic_average_flush(filters_moving_generic_average_t * ave
 	}
 }
 
-void filters_moving_generic_average_put_element(
-		filters_moving_generic_average_t * average,
+void filters_moving_average_put_element(
+		filters_moving_average_t *average,
 		int16_t new_value)
 {
+	//remove oldest value from sum and add newest one
+	average->elements_sum -= average->elements[average->index];
+	average->elements_sum += new_value;
 
-	//int32_t elem = 0;
-	//int32_t mul = 0;
+	//add new value in elements array
+	average->elements[average->index] = new_value;
 
-	uint8_t i;
-	int32_t element_sum = 0;
+	//calculate new average
+	average->avg = average->elements_sum / FILTER_MOVING_AVERAGE_ELEMENT_COUNT;
 
-
-//	printf("summing:\n");
-	//iterate over elements and move elements to next
-	for(i = average->weights_count - 1; i > 0; i--) {
-		average->elements[i] = average->elements[i - 1];							//move elements to left (older them)
-//		elem = average->elements[i];
-//		mul = ((int32_t)average->elements[i]) * average->weights[i];
-		//element_sum +=  ((int32_t)(average->elements[i]) * average->weights[i]);	//sum them up with their weight
-		element_sum += ((int32_t)average->elements[i]) * average->weights[i];
-//		printf("[e:%6ld m:%6ld s:%6ld]     ",elem, mul, element_sum);
-	}
-	//add newest element
-	//apply weight to element and add to sum
-	average->elements[0] = new_value;
-	//element_sum += (int32_t)((average->elements[0]) * average->weights[0]);
-	//printf("%6ld\n", element_sum);
-//	elem = average->elements[0];
-//	mul = ((int32_t)average->elements[0]) * average->weights[0];
-	element_sum += ((int32_t)average->elements[0]) * average->weights[0];
-//	printf("[e:%6ld m:%6ld s:%6ld]\n",elem, mul, element_sum);
-/*
-	printf("elem: ");
-	for(i = 0; i < average->weights_count; i++) {
-		printf("%6d ", average->elements[i]);
-	}
-	printf("\n");
-
-	printf("w:    ");
-	for(i = 0; i < average->weights_count; i++) {
-		printf("%6d ", average->weights[i]);
-	}
-	printf("\n");
-*/
-	average->avg = (int16_t)(element_sum / average->weights_sum);			//devide sum with weights
-//	printf("avg: %6d\n", average->avg);
-//	printf("\n\n");
-}
-
-void filters_moving_average_put_element(moving_average_t *average, int16_t value)
-{
-	average->elements_sum -= average->elements[average->index];	//zu UEberschreibenen Wert aus der Summe loeschen
-	average->elements_sum += value;							//Neuen Wert der Summe hinzufuegen
-
-	average->elements[average->index] = value;		//Neuen Wert im Array eintragen.
-
-	//Neuen Mittelwert berrechnen
-	average->mean = average->elements_sum / MOVING_AVERAGE_ELEMENT_COUNT;
-
-	//Index auf die neue Stelle schieben
-	if(average->index < MOVING_AVERAGE_ELEMENT_COUNT - 1) {
+	//move index to next element
+	if(average->index < FILTER_MOVING_AVERAGE_ELEMENT_COUNT - 1) {
 		average->index++;
 	} else {
 		average->index = 0;
 	}
 }
 
-//TODO: Replace with a more performant implementation
+void filters_moving_average_init(
+		filters_moving_average_t *average,
+		int16_t init_value)
+{
+	uint8_t i;
+
+	for(i = 0;i < FILTER_MOVING_AVERAGE_ELEMENT_COUNT;i++) {
+		filters_moving_average_put_element(average, init_value);
+	}
+}
+
+void filters_moving_average_flush(
+		filters_moving_average_t *average)
+{
+	uint8_t i;
+
+	for(i = 0;i < FILTER_MOVING_AVERAGE_ELEMENT_COUNT;i++) {
+		filters_moving_average_put_element(average, 0);
+	}
+}
+
+
+//TODO: implement a generic filter based on shift operations
+/*
 void filters_weighted_average_put_element(weighted_average_t *average, int16_t value)
 {
 	int32_t elements_sum = 0;						//sum of all elements and their weights
@@ -155,61 +154,6 @@ void filters_weighted_average_put_element(weighted_average_t *average, int16_t v
 	average->mean = elements_sum >> weighted_average_devisor;
 }
 
-void filters_smooth_put_element(smooth_t * average, int16_t value)
-{
-	average->smoothed = ((value - average->smoothed) / average->factor) + average->smoothed;
-}
-
-/*
-	for(i = 0;i < WEIGHTED_AVERAGE_ELEMENT_COUNT - 1;i++) {
-		elements_sum += average->elements[i] * weighted_average_weights[i+1];
-		average->elements[i+1] = average->elements[i];
-	}
-
-	average->elements[0] = value;
-	elements_sum += (average->elements[0] * weighted_average_weights[0]);
-	average->mean = (elements_sum / weighted_average_devisor);
 */
 
-/*//FIXME: More complex but more efficient?
-void filters_weighted_average_put_element(weighted_average_t *average, int16_t value)
-{
-	int64_t elements_sum;							//sum of all elements and their weights
-	uint8_t i;										//index for-loop
-	uint8_t weight_index = 1;						//start with second weight in for-loop for summing
-	uint8_t element_index = average->head_index;	//init element_index with last head element
 
-	//iterate (element_count - 1) times...
-	for(i = 0;i < WEIGHTED_AVERAGE_ELEMENT_COUNT - 1;i++) {
-
-		//sum elements with corresponding weight
-		elements_sum += average->elements[element_index] << weighted_average_weights[weight_index];
-
-		//select next weight
-		weight_index++;
-
-		//if at bottom of element_array set element_index to top
-		if(element_index > 0) {
-			element_index--;
-		} else {
-			element_index = WEIGHTED_AVERAGE_ELEMENT_COUNT - 1;
-		}
-	}
-
-	//add newest value to element sum
-	elements_sum += value << weighted_average_weights[0];
-
-	//make mean
-	average->mean = elements_sum >> weighted_average_devisor;
-
-	//correct head_index for new value
-	if(average->head_index < WEIGHTED_AVERAGE_ELEMENT_COUNT - 1) {
-		average->head_index++;
-	} else {
-		average->head_index = 0;
-	}
-
-	//add new value to set of elements
-	average->elements[average->head_index] = value;
-}
-*/
--- src/filters.h
+++ src/filters.h
@@ -19,31 +19,18 @@
 /* *** DECLARATIONS ********************************************************** */
 
 /* * external type and constants * */
-
-#define FILTER_MOVING_GENERIC_WEIGHTS_COUNT	8
-
-#define MOVING_AVERAGE_ELEMENT_COUNT		8
-#define WEIGHTED_AVERAGE_ELEMENT_COUNT		5
+#define FILTER_MOVING_GENERIC_WEIGHTS_COUNT		8
+#define FILTER_MOVING_AVERAGE_ELEMENT_COUNT		8
 
 typedef struct {
-	int16_t elements[MOVING_AVERAGE_ELEMENT_COUNT];
+	int16_t avg;
+	int16_t elements[FILTER_MOVING_AVERAGE_ELEMENT_COUNT];
 	int8_t  index;
 	int32_t elements_sum;
-	int16_t mean;
-} moving_average_t;
+} filters_moving_average_t;
 
 typedef struct {
-	int16_t elements[WEIGHTED_AVERAGE_ELEMENT_COUNT];
-	int16_t mean;								//sum of all elements and their weights
-} weighted_average_t;
-
-typedef struct {
-	int16_t	smoothed;
-	uint8_t factor;
-} smooth_t;
-
-typedef struct {
-	int16_t avg;								//sum of all elements and their weights
+	int16_t avg;
 	int16_t elements[FILTER_MOVING_GENERIC_WEIGHTS_COUNT];
 	uint8_t weights[FILTER_MOVING_GENERIC_WEIGHTS_COUNT];
 	uint8_t weights_count;
@@ -53,13 +40,13 @@ typedef struct {
 /* * external objects            * */
 
 /* * external functions          * */
+extern void filters_moving_generic_average_put_element(filters_moving_generic_average_t * average, int16_t new_value);
+extern void filters_moving_generic_average_init(filters_moving_generic_average_t * average, uint8_t * weights, int16_t init_value);
+extern void filters_moving_generic_average_flush(filters_moving_generic_average_t * average);
 
-void filters_moving_average_put_element(moving_average_t *average, int16_t value); /* moving_average_t *mean, int16_t value */
-void filters_weighted_average_put_element(weighted_average_t *average, int16_t value); /* moving_average_t *mean, int16_t value */
-void filters_smooth_put_element(smooth_t * average, int16_t value);
+extern void filters_moving_average_put_element(filters_moving_average_t *average, int16_t new_value); /* moving_average_t *mean, int16_t value */
+extern void filters_moving_average_init(filters_moving_average_t *average, int16_t init_value);
+extern void filters_moving_average_flush(filters_moving_average_t *average);
 
-void filters_moving_generic_average_init(filters_moving_generic_average_t * average, uint8_t * weights, uint16_t init_value);
-void filters_moving_generic_average_put_element(filters_moving_generic_average_t * average, int16_t new_value);
-void filters_moving_generic_average_flush(filters_moving_generic_average_t * average);
 
 #endif /* FILTERS_H_ */
